63a242edf7e57c6770c1b4b0e9ce2f6c93944be5
lanka <Kerr8899>
2019-06-25 08:34:17 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component, Fragment} from 'react';
+import {Provider} from 'react-redux'
+import {BrowserRouter, Route} from 'react-router-dom'
+import Header from './common/header'
+import store from './store'
+import {
+  GlobalStyle
 }
+from './assets/css/index_css.js'
+import Home from './pages/home'
+import Detail from './pages/detail/loadable'
+import Login from './pages/login'
+import Write from './pages/write'
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+      <GlobalStyle/>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Header/>
+          <Route path='/' exact component={Home}></Route>
+          <Route path='/login' exact component={Login}></Route>
+          <Route path='/write' exact component={Write}></Route>
+          <Route path='/detail/:id' exact component={Detail}></Route>
+        </BrowserRouter>
+      </Provider>
+      </Fragment>      
+    );
+  }
+}
+
 
 export default App;
